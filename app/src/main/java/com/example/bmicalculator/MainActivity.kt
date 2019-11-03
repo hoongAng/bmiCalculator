d3e7555eb9d5705a2f9006a6b347a48e088e252a
hoongAng <56661495+hoongAng@users.noreply.github.com>
2019-11-03 11:38:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSubmit.setOnClickListener {
+            calculateBMI()
+        }
+    }
+
+    private fun calculateBMI (){
+        val name = editName.text
+        val weight = editWeight.text.toString().toFloat()
+        val height = editHeight.text.toString().toDouble()
+
+        val image = imageResult
+
+        val heightInM = height/100.0
+        val bmi = weight/(Math.pow(heightInM,2.0))
+
+        if (bmi <18.5)
+        {
+            textResult.text = name.toString() + " " + getString(R.string.stringResult)+ ":${bmi}" + "\n" + getString(R.string.underweight)
+            image.visibility = View.VISIBLE
+            image.setImageResource(R.drawable.underweight)
+        }
+
+        else if (bmi > 25.0)
+        {
+            textResult.text = name.toString() + " " + getString(R.string.stringResult)+ ":${bmi}" + "\n" + getString(R.string.overweight)
+            image.visibility = View.VISIBLE
+            image.setImageResource(R.drawable.overweight)
+        }
+
+        else
+        {
+            textResult.text = name.toString() + " " + getString(R.string.stringResult)+ ":${bmi}" + "\n" + getString(R.string.normal)
+            image.visibility = View.VISIBLE
+            image.setImageResource(R.drawable.normal)
+        }
+    }
+
+    fun resetInput(view: View){
+        editName.setText("")
+        editWeight.setText("")
+        editHeight.setText("")
+        imageResult.visibility = View.GONE
+        textResult.text = ""
+    }
+}
